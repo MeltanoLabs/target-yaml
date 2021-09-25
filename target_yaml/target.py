cc7dd6204704a4c9c3beccedf1a76b10b3d93a91
Aaron ("AJ") Steers <aj@meltano.com>
2021-09-24 18:30:08 -0700

    add tap config

--- target_yaml/target.py
+++ target_yaml/target.py
@@ -14,14 +14,31 @@ class TargetYaml(Target):
     name = "target-yaml"
     config_jsonschema = th.PropertiesList(
         th.Property(
-            "filepath",
-            th.StringType,
-            description="The path to the target output file"
-        ),
-        th.Property(
             "file_naming_scheme",
             th.StringType,
-            description="The scheme with which output files will be named"
+            description=(
+                "The scheme with which output files will be named. "
+                "Naming scheme may leverage any of the following substitutions: \n\n"
+                "- `{stream_name}`"
+                "- `{datestamp}`"
+                "- `{timestamp}`"
+            ),
+        ),
+        th.Property(
+            "datestamp_format",
+            th.StringType,
+            description=(
+                "A format string to use when outputting the `{datestamp}` string. "
+                "See [TODO: tbd] for reference."
+            ),
+        ),
+        th.Property(
+            "timestamp_format",
+            th.StringType,
+            description=(
+                "A format string to use when outputting the `{timestamp}` string. "
+                "See [TODO: tbd] for reference."
+            ),
         ),
     ).to_dict()
     default_sink_class = YamlSink
